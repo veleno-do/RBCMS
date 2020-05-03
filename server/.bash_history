5494d8dd24951531d38bef9aa7fb316b99fba3bd
veleno-do <h3yukomah1y1k1m@gmail.com>
2020-05-03 13:45:07 +0900

    アクセスログのフォーマットの修正

---
+++ server/.bash_history
@@ -0,0 +1,9 @@
+exit
+cd
+ls
+cd root/rbcms
+cd ../
+ls log
+cd log
+cat access.log
+exit
--- server/root/rbcms/app/App.rb
+++ server/root/rbcms/app/App.rb
@@ -1,29 +1,29 @@
 class App
+    attr_reader :access, :parsedRequest, :logging, :socket, :infomation, :request
     public
     def self.create server
         self.new server
     end
 
     def head
-        @request.head
+        request.head
     end
 
     def content
-        @request.content
+        request.content
     end
 
     def initialize socket
         @socket = socket
         @infomation = access
         @request = parsedRequest
-        AccessLogger.paccesslog @infomation # <- logging（仮）
+        AccessLogger.paccesslog logging # <- logging（仮）
     end
 
     private
-    attr_reader :access, :parsedRequest
     def access
         arr = []
-        while req = @socket.gets.chomp
+        while req = socket.gets.chomp
             break if req == ''
             arr.push(req)
         end
@@ -31,6 +31,10 @@ class App
     end
 
     def parsedRequest
-        Request.parser @infomation
+        Request.parser infomation
+    end
+
+    def logging
+        infomation.join(' - ')
     end
 end
